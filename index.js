2ca848d50edeb4c8b09e3f929c41bcf1902d5350
basebandit <evansonmwangi83@gmail.com>
2021-07-24 22:28:31 +0300

    set node env if not set

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//Don't allow NODE_ENV to be null.\
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+// const argv = process.argv;
 const express = require("express");
 const app = express();
 
